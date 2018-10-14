a8f0fc7d479c938a952df05030aa19dc34745c23
BuildTools <unconfigured@null.spigotmc.org>
2018-10-14 16:19:07 +0900

    安定バージョンまでロールバック

--- mcRecipeSnippingTool.py
+++ mcRecipeSnippingTool.py
@@ -1,83 +1,42 @@
-#Made by Kjuman Enobikto
-#Twitter: @QmanEnobikto
-
 import os
 import PIL.Image
 import os.path
+import random
 import glob
 import shutil
 
 
-#プログラム中で使うディレクトリの作成
+if os.path.exists('pictures') == False:
+    os.mkdir('pictures')
 
-#完成したレシピ画像を入れるディレクトリ
-if os.path.exists('recipeImages') == False:
-    os.mkdir('recipeImages')
+if os.path.exists('pictemp') == False:
+    os.mkdir('pictemp')
 
-#一時的にサイズが違う画像(風景等のスクショ等)を入れておくディレクトリ
-if os.path.exists('mispictemp') == False:
-    os.mkdir('mispictemp')
+while glob.glob('*.png') != []:
+    files = []
+    files = glob.glob('*.png')
 
-#一時的にレシピ画像と思われる画像(サイズがW854H480であるもの)を入れておくディレクトリ
-if os.path.exists('recipepictemp') == False:
-    os.mkdir('recipepictemp')
+    rdcf = random.choice(files)
 
+    imga = PIL.Image.open(rdcf)
+    boxa = (304, 110, 544, 230)
+    imca = imga.crop(boxa)
+    imca.save('pictures/u_'+rdcf)
 
-#レシピ画像とそれ以外を選り分ける
-while glob.glob('*.png') != []: #ディレクトリのpng画像がなくなるまで繰り返す
-    allimg = []
-    allimg = glob.glob('*.png')
+    imgb = PIL.Image.open(rdcf)
+    boxb = (304, 240, 544, 360)
+    imcb = imgb.crop(boxb)
+    imcb.save('pictures/l_'+rdcf)
 
-    chosenImg = allimg[0] #リストallimgの一番最初を取り出す
+    shutil.move(rdcf, 'pictemp/'+rdcf)
 
-    chkImgSize = PIL.Image.open(chosenImg) #画像を開く
-    imgwidth, imgheight = chkImgSize.size #画像のWidthとHeightを取得
-    if imgwidth == 854:
-        if imgheight == 480:
-            shutil.move(chosenImg, 'recipepictemp/'+chosenImg) #画像サイズが854*480であればレシピ画像ディレクトリへ
-        else:
-            shutil.move(chosenImg, 'mispictemp/'+chosenImg) #そうでなければその他画像ディレクトリへ
-    else:
-        shutil.move(chosenImg, 'mispictemp/'+chosenImg) #そうでなければその他画像ディレクトリへ
+os.chdir('./pictemp')
 
-#作業ディレクトリをレシピ画像ディレクトリにする
-os.chdir('./recipepictemp')
+while glob.glob('*.png') != []:
+    filesa = []
+    filesa = glob.glob('*.png')
+    rdcfa = random.choice(filesa)
+    shutil.move(rdcfa, '../'+rdcfa)
 
-#画像の加工
-while glob.glob('*') != []: #中身が空になるまで繰り返す
-    recipeImages = []
-    recipeImages = glob.glob('*')
-
-    chosenRecipeImg = recipeImages[0] #リストrecipeImagesの一番最初を取り出す
-
-    #画像の上側の切り出し
-    imgAbove = PIL.Image.open(chosenRecipeImg)
-    cutImgAbove = (304, 110, 544, 230)
-    imgCuttedAbove = imgAbove.crop(cutImgAbove)
-    imgCuttedAbove.save('../recipeImages/above_'+chosenRecipeImg)
-
-    #画像の下側の切り出し
-    imgUnder = PIL.Image.open(chosenRecipeImg)
-    cutImgUnder = (304, 240, 544, 360)
-    imgCuttedUnder = imgUnder.crop(cutImgUnder)
-    imgCuttedUnder.save('../recipeImages/under_'+chosenRecipeImg)
-
-    #加工が終わった画像をその他画像ディレクトリへ
-    shutil.move(chosenRecipeImg, '../mispictemp/'+chosenRecipeImg)
-
-#作業ディレクトリをその他画像ディレクトリにする
-os.chdir('../mispictemp')
-
-#その他画像ディレクトリの中身をもとに戻す
-while glob.glob('*') != []: #中身が空になるまで繰り返す
-    allimgtemp = []
-    allimgtemp = glob.glob('*')
-
-    chosenImgs = allimgtemp[0] #リストallimgtempの一番最初を取り出す
-
-    shutil.move(chosenImgs, '../'+chosenImgs) #一つ上の階層のディレクトリ(元のディレクトリ)に画像を戻す
-
-#あとかたづけ
-os.chdir('../') #作業ディレクトリを元のディレクトリにする
-os.rmdir('./mispictemp') #作業に使ったディレクトリの削除
-os.rmdir('./recipepictemp') #作業に使ったディレクトリの削除
+os.chdir('../')
+os.rmdir('./pictemp')
